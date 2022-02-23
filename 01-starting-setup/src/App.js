92fa655b547c967c7070841de2d780db5602d104
Lucasgms <lgregory12@hotmail.com>
2022-02-23 11:47:21 -0300

    feat: [01-starting-setup] Implement expenses
    Implement expenses date and list separated component

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,10 +1,17 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    { id: 'e1', title: 'Toilet paper', amount: 94.12, date: new Date(2022, 2, 28) },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2022, 2, 28) },
+    { id: 'e3', title: 'Car insurance', amount: 294.67, date: new Date(2022, 2, 28) },
+    { id: 'e4', title: 'Car insurance', amount: 294.67, date: new Date(2022, 2, 28) },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      <Expenses expenses={expenses} />
     </div>
   );
 }
